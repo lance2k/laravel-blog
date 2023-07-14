2af8d628d408d9acd30ecb3ae2832b850e20fbab
lance2k <mhoneylance@gmail.com>
2023-07-14 19:30:39 +0800

    Ep 24 Your First Eloquent Relationship

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,6 +2,9 @@
     @foreach ($posts as $post)
     <article>
         <h1><a href="posts/{{$post->slug}}">{{$post->title}}</a></h1>
+        <p>
+            <a href="#">{{ $post->category->name }}</a>
+        </p>
         <div>
             {{$post->excerpt}}
         </div>
