656b450ed813cd575a807137f85f16f597bdf900
lance2k <mhoneylance@gmail.com>
2023-07-14 19:53:24 +0800

    Ep 25 Show All Posts Associated With a Category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
     @foreach ($posts as $post)
     <article>
-        <h1><a href="posts/{{$post->slug}}">{{$post->title}}</a></h1>
+        <h1><a href="/posts/{{$post->slug}}">{{$post->title}}</a></h1>
         <p>
-            <a href="#">{{ $post->category->name }}</a>
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
         </p>
         <div>
             {{$post->excerpt}}
