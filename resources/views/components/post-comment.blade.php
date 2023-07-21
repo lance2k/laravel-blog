14b7d263fb31fc4cc760a731ed680ccf37026b22
Lance <mhoneylance@gmail.com>
2023-07-21 17:10:46 +0800

    Ep 54 Make the Comments Section Dynamic

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,16 +1,15 @@
+@props(['comment'])
 <article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img class="rounded-xl" src="https://i.pravatar.cc/60" width="60" height="60" alt="Avatar">
+        <img class="rounded-xl" src="https://i.pravatar.cc/60?u={{ $comment->id }}" width="60" height="60" alt="Avatar">
     </div>
     <div>
         <header class="mb-4">
-            <h3 class="font-bold">John Doe</h3>
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
             <p class="text-xs">
-                Posted <time>8 months ago</time>
+                Posted <time>{{ $comment->created_at }}</time>
             </p>
         </header>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae doloribus
-            corporis saepe recusandae doloremque atque? Nobis hic ipsam ullam perferendis
-            dolorum? Error maiores natus eius praesentium doloribus, minus at repellendus.</p>
+        <p>{{ $comment->body }}</p>
     </div>
 </article>
